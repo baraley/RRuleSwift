bc0cf52bfe1829e45aa162bfcd8f5e0715b8f792
Oleksandr Baralei <alexander.baraley@gmail.com>
2024-07-24 18:49:43 +0300

    Fix search of path to package's resources

--- Package.swift
+++ Package.swift
@@ -6,18 +6,21 @@ import PackageDescription
 let package = Package(
     name: "RRuleSwift",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RRuleSwift",
-            targets: ["RRuleSwift"]),
+            targets: ["RRuleSwift"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RRuleSwift"),
+            name: "RRuleSwift",
+            resources: [
+              .process("lib")
+            ]
+        ),
         .testTarget(
             name: "RRuleSwiftTests",
-            dependencies: ["RRuleSwift"]),
+            dependencies: ["RRuleSwift"]
+        ),
     ]
 )
--- Tests/RRuleSwiftTests/RRuleSwiftTests.swift
+++ Tests/RRuleSwiftTests/RRuleSwiftTests.swift
@@ -2,11 +2,8 @@ import XCTest
 @testable import RRuleSwift
 
 final class RRuleSwiftTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testExample() throws {
+      let file = Bundle.module.url(forResource: "rrule", withExtension: "js")
     }
 }
